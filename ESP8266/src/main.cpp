d0df349b8e94999ae1cb8b4fdfdb594b2c1f3313
Geoff <geoff.day@matchesfashion.com>
2025-12-24 18:06:32 +0000

    Update state

--- ESP8266/src/main.cpp
+++ ESP8266/src/main.cpp
@@ -18,6 +18,7 @@
 
 #define SETTINGS_TOPIC ROOT_TOPIC "/settings"
 #define UPDATE_SETTINGS_TOPIC ROOT_TOPIC "/update-settings"
+#define UPDATE_STATE_TOPIC ROOT_TOPIC "/update-state"
 
 #define FAN_PIN 4
 #define ONE_WIRE_BUS 13
@@ -33,24 +34,68 @@ OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 
 espMqttClient mqttClient;
-const char mqttHost[] = "192.168.68.106";
+const char mqttHost[] = "loft.local";
 const int mqttPort = 1883;
 char mqttMessage[256];
 
 int state;
 float readings[5];
-int readingIndex = 0;
+int readingIndex;
+unsigned long fanOnTime;
+unsigned long lastMsg;
 
-// Settings
 struct Settings {
   int signature;
   float onTemperature, onRate, offTemperature;
   int minimumMinutes;
 } settings;
 
-unsigned long lastMsg = 0;
-#define MSG_BUFFER_SIZE (50)
-char msg[MSG_BUFFER_SIZE];
+void publish(const char* ptopic, const float value) {
+  char s[32];
+
+  sprintf(s, "%0.1f", value);
+  mqttClient.publish(ptopic, 1, true, s);
+}
+
+void publish(const char* ptopic, const int value) {
+  char s[32];
+
+  sprintf(s, "%d", value);
+  mqttClient.publish(ptopic, 1, true, s);
+}
+
+void publish(const char* ptopic, const char* pvalue) {
+  mqttClient.publish(ptopic, 1, true, pvalue);
+}
+
+void fanOn() {
+  digitalWrite(FAN_PIN, 1);
+  publish(CURRENT_EXTRACTOR_TOPIC, "on");
+}
+
+void fanOff() {
+  digitalWrite(FAN_PIN, 0);
+  publish(CURRENT_EXTRACTOR_TOPIC, "off");
+}
+
+void transition(int newState) {
+  switch (newState) {
+    case STATE_IDLE:
+      fanOff();
+      state = STATE_IDLE;
+      break;
+
+    case STATE_INITIAL:
+      fanOn();
+      fanOnTime = millis();
+      state = STATE_INITIAL;
+      break;
+
+    case STATE_WAITING:
+      state = STATE_WAITING;
+      break;
+  }
+}
 
 float rateOfChange() {
   if (readingIndex < 5) {
@@ -105,34 +150,6 @@ void handleSubscribed(uint16_t packetId, const espMqttClientTypes::SubscribeRetu
   Serial.printf("MQTT subscribed\n");
 }
 
-void fanOn() {
-  digitalWrite(FAN_PIN, 1);
-  if (!mqttClient.publish(CURRENT_EXTRACTOR_TOPIC, 1, true, "on")) {
-    ESP.restart();
-  }
-}
-
-void fanOff() {
-  digitalWrite(FAN_PIN, 0);
-  if (!mqttClient.publish(CURRENT_EXTRACTOR_TOPIC, 1, true, "off")) {
-    ESP.restart();
-  }
-}
-
-void publish(const char *ptopic, const float value) {
-  char s [32];
-
-  sprintf (s, "%0.1f", value);
-  mqttClient.publish(ptopic, 1, true, s);
-}
-
-void publish(const char *ptopic, const int value) {
-  char s [32];
-
-  sprintf (s, "%d", value);
-  mqttClient.publish(ptopic, 1, true, s);
-}
-
 void dumpSettings() {
   Serial.println("SETTINGS");
   Serial.printf(" On temperature: %0.1f\n", settings.onTemperature);
@@ -170,10 +187,10 @@ void procesSettings(const char* psettings) {
   JsonDocument doc;
   deserializeJson(doc, psettings);
 
-  settings.onTemperature = doc["onTemperature"];
-  settings.onRate = doc["onRate"];
-  settings.offTemperature = doc["offTemperature"];
-  settings.minimumMinutes = doc["minimumMinutes"];
+  settings.onTemperature = doc["on-temperature"];
+  settings.onRate = doc["on-rate"];
+  settings.offTemperature = doc["off-temperature"];
+  settings.minimumMinutes = doc["minimum-minutes"];
 
   EEPROM.put(0, settings);
   EEPROM.commit();
@@ -181,18 +198,28 @@ void procesSettings(const char* psettings) {
   dumpSettings();
 }
 
+void processState(const char *pstate) {
+  if (!strcmp(pstate, "idle")) {
+    transition(STATE_IDLE);
+    lastMsg = 0;
+  } else if (!strcmp(pstate, "initial")) {
+    transition(STATE_INITIAL);
+    lastMsg = 0;
+  } else if (!strcmp(pstate, "waiting")) {
+    transition(STATE_WAITING);
+    lastMsg = 0;
+  } else {
+    Serial.printf("Unknown state: %s\n", pstate);
+  }
+}
+
 void handleMessage(const espMqttClientTypes::MessageProperties& properties, const char* topic, const uint8_t* payload, size_t len, size_t index, size_t total) {
   Serial.printf("MQTT message received for topic: %s\n", topic);
 
-  if (!strcmp(topic, "shower/override")) {
+  if (!strcmp(topic, UPDATE_STATE_TOPIC)) {
     memcpy(mqttMessage, payload, len);
     mqttMessage[len] = 0;
-    Serial.printf("MQTT message: %s\n", mqttMessage);
-    if (!strcmp(mqttMessage, "on")) {
-      fanOn();
-    } else if (!strcmp(mqttMessage, "off")) {
-      fanOff();
-    }
+    processState(mqttMessage);
   } else if (!strcmp(topic, UPDATE_SETTINGS_TOPIC)) {
     memcpy(mqttMessage, payload, len);
     mqttMessage[len] = 0;
@@ -217,7 +244,7 @@ void connectMqtt() {
     delay(100);
   }
 
-  // mqttClient.subscribe("shower/override", 1);
+  mqttClient.subscribe(UPDATE_STATE_TOPIC, 1);
   mqttClient.subscribe(UPDATE_SETTINGS_TOPIC, 1);
 }
 
@@ -228,15 +255,15 @@ void setup() {
 
   EEPROM.begin(64);
 
-  fanOff();
-  pinMode(FAN_PIN, OUTPUT);
-  Serial.println("Extractor initialised");
-
   connectWiFi();
   Serial.println("WiFi initialised");
 
   connectMqtt();
   Serial.println("MQTT initialised");
+
+  fanOff();
+  pinMode(FAN_PIN, OUTPUT);
+  Serial.println("Extractor initialised");
 
   loadSettings();
   Serial.println("Settings initialised");
@@ -249,10 +276,6 @@ void setup() {
   state = STATE_IDLE;
   Serial.println("State initialised");
 }
-
-// Smoothed rate of change > 10, min 5 minutes, then until temp < 20
-
-unsigned long fanOnTime;
 
 void loop() {
   float tempC, roc;
@@ -270,67 +293,46 @@ void loop() {
     recordReading(tempC);
     roc = rateOfChange();
 
-    snprintf(msg, MSG_BUFFER_SIZE, "%0.1f", roc);
-    if (!mqttClient.publish(CURRENT_RATE_TOPIC, 1, true, msg)) {
-      ESP.restart();
-    }
+    publish(CURRENT_RATE_TOPIC, roc);
+    publish(CURRENT_TEMPERATURE_TOPIC, tempC);
 
-    snprintf(msg, MSG_BUFFER_SIZE, "%0.1f", tempC);
-    if (!mqttClient.publish(CURRENT_TEMPERATURE_TOPIC, 1, true, msg)) {
-      ESP.restart();
-    }
+    // Serial.printf("Temp: %0.1f, rate of change: %0.1f, current state: %d\n", tempC, roc, state);
 
-    Serial.printf("Temp: %0.1f, rate of change: %0.1f, current state: %d\n", tempC, roc, state);
+    // Always watch for rapidly increasing temperature whatever the current state.
+    if (roc > settings.onRate) {
+      transition(STATE_INITIAL);
+    }
 
     switch (state) {
       case STATE_IDLE:
-        if (!mqttClient.publish(CURRENT_STATE_TOPIC, 1, true, "idle")) {
-          ESP.restart();
-        }
+        publish(CURRENT_STATE_TOPIC, "idle");
 
-        // Idle state, if we see the temp go up fast or the temp rises too much then turn the fan on cos the water's flowing.
-        if (roc > settings.onRate || tempC > settings.onTemperature) {
-          fanOn();
-          fanOnTime = millis();
-          state = STATE_INITIAL;
+        // Check for high temperature and start the cycle if found.
+        if (tempC > settings.onTemperature) {
+          transition(STATE_INITIAL);
         }
 
         break;
 
       case STATE_INITIAL:
-        if (!mqttClient.publish(CURRENT_STATE_TOPIC, 1, true, "initial")) {
-          ESP.restart();
-        }
+        publish(CURRENT_STATE_TOPIC, "initial");
 
-        // We've just turned the fan on, keep it on a period, resetting the timer if there's another ramp up in the temperature.
-        if (roc > settings.onRate) {
-          fanOnTime = millis();
-          break;
-        }
-
+        // We've just turned the fan on, keep it on for a period.
         if ((millis() - fanOnTime) > 1000UL * 60UL * settings.minimumMinutes) {
-          state = STATE_WAITING;
+          transition(STATE_WAITING);
         }
 
         break;
 
       case STATE_WAITING:
-        if (!mqttClient.publish(CURRENT_STATE_TOPIC, 1, true, "waiting")) {
-          ESP.restart();
-        }
+        publish(CURRENT_STATE_TOPIC, "waiting");
 
-        // Initial delay is over, wait for the temperature to be low enough to switch the fan off, unless there's another ramp up.
-        if (roc > settings.onRate) {
-          fanOnTime = millis();
-          state = STATE_INITIAL;
-          break;
-        }
-
+        // Initial delay is over, wait for the temperature to be low enough to switch the fan off.
         if (tempC < settings.offTemperature) {
-          fanOff();
-          state = STATE_IDLE;
-          break;
+          transition(STATE_IDLE);
         }
+
+        break;
     }
 
     lastMsg = now;
