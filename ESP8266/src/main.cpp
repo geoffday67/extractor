bd43dfc71f721961157197e44ae5493d1f03803a
Geoff <geoff.day@matchesfashion.com>
2025-12-24 16:31:14 +0000

    Update and store settings

--- ESP8266/src/main.cpp
+++ ESP8266/src/main.cpp
@@ -1,12 +1,18 @@
+#include <ArduinoJson.h>
 #include <DallasTemperature.h>
+#include <EEPROM.h>
 #include <ESP8266WiFi.h>
 #include <OneWire.h>
 #include <espMqttClient.h>
 
-#define ON_TEMPERATURE 26
-#define ON_RATE 10
-#define OFF_TEMPERATURE 24
-#define MINIMUM_MINUTES 5
+#define MQTT_CLIENT "shower-test"
+
+#define ROOT_TOPIC "shower-test"
+#define STATE_TOPIC ROOT_TOPIC "/state"
+#define RATE_TOPIC ROOT_TOPIC "/rate"
+#define TEMPERATURE_TOPIC ROOT_TOPIC "/temperature"
+#define EXTRACTOR_TOPIC ROOT_TOPIC "/extractor"
+#define SETTINGS_TOPIC ROOT_TOPIC "/settings"
 
 #define FAN_PIN 4
 #define ONE_WIRE_BUS 13
@@ -24,12 +30,18 @@ DallasTemperature sensors(&oneWire);
 espMqttClient mqttClient;
 const char mqttHost[] = "192.168.68.106";
 const int mqttPort = 1883;
-const char mqttClientId[] = "shower";
 char mqttMessage[256];
 
 int state;
 float readings[5];
 int readingIndex = 0;
+
+// Settings
+struct Settings {
+  int signature;
+  float onTemperature, onRate, offTemperature;
+  int minimumMinutes;
+} settings;
 
 unsigned long lastMsg = 0;
 #define MSG_BUFFER_SIZE (50)
@@ -90,20 +102,64 @@ void handleSubscribed(uint16_t packetId, const espMqttClientTypes::SubscribeRetu
 
 void fanOn() {
   digitalWrite(FAN_PIN, 1);
-  if (!mqttClient.publish("shower/extractor", 1, true, "on")) {
+  if (!mqttClient.publish(EXTRACTOR_TOPIC, 1, true, "on")) {
     ESP.restart();
   }
 }
 
 void fanOff() {
   digitalWrite(FAN_PIN, 0);
-  if (!mqttClient.publish("shower/extractor", 1, true, "off")) {
+  if (!mqttClient.publish(EXTRACTOR_TOPIC, 1, true, "off")) {
     ESP.restart();
   }
 }
 
+void dumpSettings() {
+  Serial.println("SETTINGS");
+  Serial.printf(" On temperature: %0.1f\n", settings.onTemperature);
+  Serial.printf(" On rate: %0.1f\n", settings.onRate);
+  Serial.printf(" Off temperature: %0.1f\n", settings.offTemperature);
+  Serial.printf(" Minimum minutes: %d\n", settings.minimumMinutes);
+}
+
+void loadSettings() {
+  EEPROM.get(0, settings);
+
+  if (settings.signature != 12345) {
+    // This is the first time we've loaded the settings, set some sensible defaults.
+    Serial.println("Saved settings not found, using defaults");
+
+    settings.signature = 12345;
+    settings.onTemperature = 25;
+    settings.onRate = 10;
+    settings.offTemperature = 23;
+    settings.minimumMinutes = 5;
+
+    EEPROM.put(0, settings);
+    EEPROM.commit();
+  }
+
+  dumpSettings();
+}
+
+void procesSettings(const char* psettings) {
+  JsonDocument doc;
+  deserializeJson(doc, psettings);
+
+  settings.onTemperature = doc["onTemperature"];
+  settings.onRate = doc["onRate"];
+  settings.offTemperature = doc["offTemperature"];
+  settings.minimumMinutes = doc["minimumMinutes"];
+
+  EEPROM.put(0, settings);
+  EEPROM.commit();
+
+  dumpSettings();
+}
+
 void handleMessage(const espMqttClientTypes::MessageProperties& properties, const char* topic, const uint8_t* payload, size_t len, size_t index, size_t total) {
   Serial.printf("MQTT message received for topic: %s\n", topic);
+
   if (!strcmp(topic, "shower/override")) {
     memcpy(mqttMessage, payload, len);
     mqttMessage[len] = 0;
@@ -113,6 +169,10 @@ void handleMessage(const espMqttClientTypes::MessageProperties& properties, cons
     } else if (!strcmp(mqttMessage, "off")) {
       fanOff();
     }
+  } else if (!strcmp(topic, SETTINGS_TOPIC)) {
+    memcpy(mqttMessage, payload, len);
+    mqttMessage[len] = 0;
+    procesSettings(mqttMessage);
   } else {
     Serial.println("Unknown topic");
   }
@@ -121,7 +181,7 @@ void handleMessage(const espMqttClientTypes::MessageProperties& properties, cons
 void connectMqtt() {
   mqttClient
       .setServer(mqttHost, mqttPort)
-      .setClientId(mqttClientId)
+      .setClientId(MQTT_CLIENT)
       .setCleanSession(false)
       .onConnect(handleMqttConnected)
       .onSubscribe(handleSubscribed)
@@ -133,13 +193,18 @@ void connectMqtt() {
     delay(100);
   }
 
-  mqttClient.subscribe("shower/override", 1);
+  // mqttClient.subscribe("shower/override", 1);
+  mqttClient.subscribe(SETTINGS_TOPIC, 1);
 }
 
 void setup() {
   Serial.begin(115200);
   Serial.println();
   Serial.println("Starting");
+
+  EEPROM.begin(64);
+  loadSettings();
+  Serial.println("Settings initialised");
 
   fanOff();
   pinMode(FAN_PIN, OUTPUT);
@@ -180,12 +245,12 @@ void loop() {
     roc = rateOfChange();
 
     snprintf(msg, MSG_BUFFER_SIZE, "%0.1f", roc);
-    if (!mqttClient.publish("shower/rate", 1, true, msg)) {
+    if (!mqttClient.publish(RATE_TOPIC, 1, true, msg)) {
       ESP.restart();
     }
 
     snprintf(msg, MSG_BUFFER_SIZE, "%0.1f", tempC);
-    if (!mqttClient.publish("shower/temperature", 1, true, msg)) {
+    if (!mqttClient.publish(TEMPERATURE_TOPIC, 1, true, msg)) {
       ESP.restart();
     }
 
@@ -193,12 +258,12 @@ void loop() {
 
     switch (state) {
       case STATE_IDLE:
-        if (!mqttClient.publish("shower/state", 1, true, "idle")) {
+        if (!mqttClient.publish(STATE_TOPIC, 1, true, "idle")) {
           ESP.restart();
         }
 
         // Idle state, if we see the temp go up fast or the temp rises too much then turn the fan on cos the water's flowing.
-        if (roc > ON_RATE || tempC > ON_TEMPERATURE) {
+        if (roc > settings.onRate || tempC > settings.onTemperature) {
           fanOn();
           fanOnTime = millis();
           state = STATE_INITIAL;
@@ -207,35 +272,35 @@ void loop() {
         break;
 
       case STATE_INITIAL:
-        if (!mqttClient.publish("shower/state", 1, true, "initial")) {
+        if (!mqttClient.publish(STATE_TOPIC, 1, true, "initial")) {
           ESP.restart();
         }
 
         // We've just turned the fan on, keep it on a period, resetting the timer if there's another ramp up in the temperature.
-        if (roc > ON_RATE) {
+        if (roc > settings.onRate) {
           fanOnTime = millis();
           break;
         }
 
-        if ((millis() - fanOnTime) > 1000 * 60 * MINIMUM_MINUTES) {
+        if ((millis() - fanOnTime) > 1000UL * 60UL * settings.minimumMinutes) {
           state = STATE_WAITING;
         }
 
         break;
 
       case STATE_WAITING:
-        if (!mqttClient.publish("shower/state", 1, true, "waiting")) {
+        if (!mqttClient.publish(STATE_TOPIC, 1, true, "waiting")) {
           ESP.restart();
         }
 
         // Initial delay is over, wait for the temperature to be low enough to switch the fan off, unless there's another ramp up.
-        if (roc > ON_RATE) {
+        if (roc > settings.onRate) {
           fanOnTime = millis();
           state = STATE_INITIAL;
           break;
         }
 
-        if (tempC < OFF_TEMPERATURE) {
+        if (tempC < settings.offTemperature) {
           fanOff();
           state = STATE_IDLE;
           break;
